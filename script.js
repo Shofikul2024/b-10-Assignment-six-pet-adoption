d4552cbff5c40d32005c688f749a85da27c96a5e
Shofikul2024 <islamshofi942@gmail.com>
2025-02-19 20:52:29 +0600

    added display categories  part

--- script.js
+++ script.js
@@ -152,4 +152,60 @@ const displayAllPets = (pets) => {
     petContainer.classList.add("grid");
   }
 
-   
+  pets.forEach((pet) => {
+    const card = document.createElement("div");
+    card.classList = "card ";
+    card.innerHTML = `
+    <div class="border border-gray-400 rounded-xl p-4">
+     <figure class="h-[160px]">
+    <img
+    class="h-full w-full object-cover"
+      src="${pet.image}"
+      alt="" />
+  </figure>
+  <div class="card-body px-0">
+    <h2 class="card-title">${pet.pet_name}</h2>
+    <p class="flex items-center gap-2"><img src="https://img.icons8.com/?size=16&id=68585&format=png"> Breed: ${
+      pet.breed ? pet.breed : "Not available"
+    }</p>
+    <p class="flex items-center gap-2"><img class="w-4" src="https://img.icons8.com/?size=24&id=84997&format=png"> Birth: ${
+      pet.date_of_birth ? pet.date_of_birth : "Not available"
+    }</p>
+    <p class="flex items-center gap-2"><img class="w-4" src="https://img.icons8.com/?size=32&id=16275&format=png"> Gender: ${
+      pet.gender ? pet.gender : "Not available"
+    }</p>
+    <p class="flex items-center gap-2"><img class="w-4" src="https://img.icons8.com/?size=24&id=85782&format=png"> Price: ${
+      pet.price
+    }$ </p>
+  </div>
+  <hr>
+  <div class="card-actions flex justify-between mt-3 items-center">
+      <button onclick="loadPetPhotos(${
+        pet.petId
+      })" class="border border-gray-400 rounded-lg p-2 hover:bg-gray-300"><img src="https://img.icons8.com/?size=24&id=82788&format=png"></button>
+      <button onclick="openModal()" class="font-bold text-lg text-[#0E7A81] border border-gray-400 rounded-lg p-2 hover:bg-gray-300">Adopt</button>
+      <button onclick="loadPetDetails(${
+        pet.petId
+      })" class="font-bold text-lg text-[#0E7A81] border border-gray-400 rounded-lg p-2 hover:bg-gray-300">Details</button>
+    </div>
+  </div>
+        `;
+    petContainer.append(card);
+  });
+};
+
+// display categories
+const displayCategories = (categories) => {
+  const categoryContainer = document.getElementById("category");
+
+  categories.forEach((item) => {
+    const button = document.createElement("button");
+    button.innerHTML = `
+        <button id="btn-${item.category}" onclick="loadCategoryPets('${item.category}')" class="flex items-center gap-2 p-4 font-bold text-2xl border rounded-2xl hover:bg-gray-300 category-btn"><img class="w-6" src="${item.category_icon}"> ${item.category}</button>
+        `;
+    categoryContainer.append(button);
+  });
+};
+
+loadCategories();
+loadAllPets();

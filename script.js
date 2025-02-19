1912f8027ec6c9532ea6fbd9aaea4ac9c824e8ee
Shofikul2024 <islamshofi942@gmail.com>
2025-02-19 20:24:56 +0600

    added main section within text content .

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 console.log("hello");
 
+const removeActiveClass = () => {
+  const buttons = document.getElementsByClassName("category-btn");
+  for (let btn of buttons) {
+    btn.classList.remove("active");
+  }
+};
+
+// load categories
+const loadCategories = () => {
+  fetch("https://openapi.programming-hero.com/api/peddy/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((error) => console.log(error));
+};
  
